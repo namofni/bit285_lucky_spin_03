065de13cbc97c86169ab42dc9d879800bdd5a0e0
Wing <namofni@hotmail.com>
2019-01-21 21:28:49 -0800

    My finish
    My finished changes including adding title change

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LuckySpin.Models;
 
 namespace LuckySpin.Controllers
 {
@@ -11,24 +12,52 @@ namespace LuckySpin.Controllers
         Random random = new Random();
 
         //TODO: Update the Conroller to receive the Luck number from the Route
-        public IActionResult Index()
+        public IActionResult Index(int luck)
         {
+            Spin spin = new Spin();
             int a = random.Next(1, 10);
             int b = random.Next(1, 10);
             int c = random.Next(1, 10);
 
-            if (a == 7 || b == 7 || c == 7)
-                ViewBag.Display = "block";
+            if (a == luck || b == luck || c == luck)
+                spin.Image = "block";
             else
-                ViewBag.Display = "none";
+                spin.Image = "none";
+
+            spin.A = a;
+            spin.B = b;
+            spin.C = c;
+
+            switch(luck)
+            {
+                default:
+                    spin.Title = "Nothing";
+                    break;
+                case 1:
+                    spin.Title = "One";
+                    break;
+                case 2:
+                    spin.Title = "Two";
+                    break;
+                case 3:
+                    spin.Title = "Three";
+                    break;
+                case 4:
+                    spin.Title = "Four";
+                    break;
+                case 5:
+                    spin.Title = "Five";
+                    break;
+                case 6:
+                    spin.Title = "Six";
+                    break;
+                case 7:
+                    spin.Title = "Seven";
+                    break;
+            }
 
 
-            ViewBag.A = a;
-            ViewBag.B = b;
-            ViewBag.C = c;
-
-
-            return View();
+            return View(spin);
         }
     }
 }
---
+++ LuckySpin/Models/Spin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace LuckySpin.Models
+{
+    public class Spin
+    {
+        public int Luck { get; set; }
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+    }
+}
